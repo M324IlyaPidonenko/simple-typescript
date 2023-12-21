453b45b2197efbd6b36b9374476675899270d113
ilya Pidonenko <ilja.pidonekom@gmail.com>
2023-12-21 13:21:16 +0100

    REST-Endpunkt added for Timestamps

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import http from 'http';
 import { Request, Response } from 'express';
 import os from 'os';
+import { format } from 'date-fns';
 
 // Create the express server
 const app = express();
@@ -10,6 +11,15 @@ const server = http.createServer(app);
 // route for / that returns a simple html page with the hostname
 app.get('/', (req: Request, res: Response) => {
   res.send(`<h1>Hello World!</h1><p>Host: ${os.hostname()}</p>`);
+});
+
+app.get('/log/time', (req: Request, res: Response) => {
+  const currentTime = new Date();
+  const formattedTime = format(currentTime, 'yyyy-MM-dd HH:mm:ss');
+
+  console.log(`Request received at /log/time. Current time: ${formattedTime}`);
+
+  res.send(`<p>Logged current time: ${formattedTime}</p>`);
 });
 
 const serverPort = process.env.PORT || 3000;
